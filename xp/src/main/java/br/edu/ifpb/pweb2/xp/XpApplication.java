883caad94e4b6b366a1728244ce8ae5550bf519d
Matheus Barbosa <115056575+themattery@users.noreply.github.com>
2026-05-01 13:38:20 -0300

    Initial commit

---
+++ xp/src/main/java/br/edu/ifpb/pweb2/xp/XpApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.xp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpApplication.class, args);
+	}
+
+}
