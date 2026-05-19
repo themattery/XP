2c5f8c3e5518d7ec191b7abaa2c7a70b26049878
Luizalima3 <maria.lima.5@academico.ifpb.edu.br>
2026-05-19 00:40:26 -0300

    fix: corrige salvamento do ranking ao finalizar corrida

---
+++ xp/src/main/java/br/edu/ifpb/pweb2/xp/repository/ParticipanteRepository.java
@@ -0,0 +1,12 @@
+package br.edu.ifpb.pweb2.xp.repository;
+
+import br.edu.ifpb.pweb2.xp.model.Participante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ParticipanteRepository extends JpaRepository<Participante, Long> {
+    Optional<Participante> findByNome(String nome);
+}
---
+++ xp/src/main/java/br/edu/ifpb/pweb2/xp/service/ParticipanteService.java
@@ -0,0 +1,28 @@
+package br.edu.ifpb.pweb2.xp.service;
+
+import br.edu.ifpb.pweb2.xp.model.Participante;
+import br.edu.ifpb.pweb2.xp.repository.ParticipanteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ParticipanteService {
+
+    @Autowired
+    private ParticipanteRepository repository;
+
+    public Participante buscarOuCriar(String nome) {
+        Optional<Participante> existente = repository.findByNome(nome);
+        
+        if (existente.isPresent()) {
+            return existente.get();
+        }
+        
+        Participante novo = new Participante();
+        novo.setNome(nome);
+        novo.setAdmin("admin".equalsIgnoreCase(nome));
+        return repository.save(novo);
+    }
+}
